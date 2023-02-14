53e471c223fca010c6b22a99247064cac5d863f6
Parinchat Juntaworn <j.parinchat@gmail.com>
2023-02-14 11:49:56 +0700

    system finish but doing validate form

--- index.js
+++ index.js
@@ -1,18 +1,100 @@
-// แต่ง = 8 90
-// บวช = 7 80
-// ศพ = 10 100
-// ทอดผ้าป่า = 6 60
-// งานทำบุญใหญ่ = 9 70
-// ทำบุญขึ้นบ้านใหม่ = 8 70
-// งานรับปริญญา = 4 10
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let totalPoint = 0;
 
+  // Get event point
+  let el = event.target;
+  let getEventPoint = el.querySelector(".con-event #event").value;
 
-// function to get result
-const getResult = () => {
-    let result = document.getElementById('preResult')
-    result.innerText = 'CHANGE!' // เปลี่ยนเป็นคำตอบจำนวนเงินที่รับมาจาก function อื่น
-}
+  switch (getEventPoint) {
+    case "งานแต่งงาน":
+      totalPoint += 9;
+      break;
+    case "งานบวช":
+      totalPoint += 7;
+      break;
+    case "งานศพ":
+      totalPoint += 10;
+      break;
+    case "งานทอดผ้าป่า":
+      totalPoint += 6;
+      break;
+    case "งานทำบุญใหญ่":
+      totalPoint += 9;
+      break;
+    case "งานทำบุญขึ้นบ้านใหม่":
+      totalPoint += 8;
+      break;
+    case "งานรับปริญญา":
+      totalPoint += 5;
+      break;
+  }
 
-// click to exhibit result
-let calResult = document.getElementById('clickToResult')
-calResult.addEventListener('click', getResult)
+  // Get important point
+  let getImportantPoint = el.querySelector(".con-important #important").value;
+
+  if (getImportantPoint === "มาก") {
+    totalPoint += 3;
+  } else if (getImportantPoint === "ปานกลาง") {
+    totalPoint += 2;
+  } else if (getImportantPoint === "น้อย") {
+    totalPoint += 1;
+  }
+
+  // Get join point
+  let getJoinPoint = el.querySelector(".con-join #join").value;
+
+  switch (getJoinPoint) {
+    case "ไปร่วมงาน":
+      totalPoint += 1;
+      break;
+    case "ไม่ไปร่วมงาน":
+      totalPoint += 3;
+      break;
+    case "มีตัวแทนไปร่วมงาน":
+      totalPoint += 2;
+      break;
+  }
+
+  // Get income point
+  let getIncomePoint = el.querySelector(".con-income #income").value;
+
+  if (getIncomePoint <= 15000) {
+    totalPoint += 1;
+  } else if (getIncomePoint > 15000 && getIncomePoint <= 30000) {
+    totalPoint += 2;
+  } else if (getIncomePoint > 30000) {
+    totalPoint += 3;
+  }
+
+  console.log(totalPoint);
+
+  // Display result
+  let saiSong;
+  switch (totalPoint) {
+    case 8:
+      saiSong = '50';
+      break;
+    case 9:
+      saiSong = '100';
+      break;
+    case 10 && 11:
+      saiSong = '250';
+      break;
+    case 12 && 13:
+      saiSong = '300';
+      break;
+    case 14 && 15:
+      saiSong = '500';
+      break;
+    case 16 && 17:
+      saiSong = '800';
+      break;
+    case 18 && 19:
+      saiSong = '1000';
+      break;
+  }
+
+  document.getElementById("preResult").innerText = saiSong
+});
